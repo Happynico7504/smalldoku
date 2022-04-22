a42cf4cb1b4d9813b7c93fd3768047b5fb002e3e
Happynico7504 <77099168+Happynico7504@users.noreply.github.com>
2022-04-22 11:44:28 +0200

    Update setup-edk.sh

--- setup-edk.sh
+++ setup-edk.sh
@@ -1,4 +1,4 @@
-apt-get install build-essential git uuid-dev iasl nasm
+apt-get install build-essential git uuid-dev iasl nasm gnu-efi
 git clone git://github.com/tianocore/edk2.git
 cd edk2
 make -C BaseTools
